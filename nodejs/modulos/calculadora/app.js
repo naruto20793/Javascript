52c1e8fe43e63704b8754bd7801300e5ab214977
Guilherme Pereira Mondardo <guilhermepmondardo@gmail.com>
2025-10-03 09:55:37 -0300

    30/09/25

--- nodejs/modulos/calculadora/app.js
+++ nodejs/modulos/calculadora/app.js
@@ -1,6 +1,6 @@
 //const funcoesMatematicas = require('./funcoes_matematicas');
-import {somar,subtrair,multiplicar,dividir} from './funcoes_matematicas.js';
-import * as calculadora_idade from './calculadora_idade.js';
+import {somar,subtrair,multiplicar,dividir} from './modulos/funcoes_matematicas.js';
+import * as calculadora_idade from './modulos/calculadora_idade.js';
 
 
 // Exemplo de uso:
